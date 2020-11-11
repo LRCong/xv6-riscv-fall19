e30247481a8b8ff1eafe01c4243724b9c864da6f
LRCong <1323794482@qq.com>
2020-11-11 16:29:42 +0800

    true last commit for sh

--- user/nsh.c
+++ user/nsh.c
@@ -9,7 +9,7 @@ int getcmd(char *buf, int nbuf)
     fprintf(2, "@ ");
     memset(buf, 0, nbuf);
     gets(buf, nbuf);
-    if (buf[0] == 0)
+    if (buf[0] == '\0')
         return -1;
     return 0;
 }
